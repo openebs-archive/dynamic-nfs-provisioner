c7533a45cc1e19c798633498b615b80360aa9dc6
mayank <mayank.patel@mayadata.io>
2021-07-21 18:34:02 +0530

    bdd(retain-backendPv): BDD test to verify the existence of backend PV having retainPolicy set to Reclaim
    Signed-off-by: mayank <mayank.patel@mayadata.io>

--- tests/k8s_utils.go
+++ tests/k8s_utils.go
@@ -230,6 +230,14 @@ func (k *KubeClient) deletePVC(namespace, pvc string) error {
 	return err
 }
 
+func (k *KubeClient) getPV(pvName string) (*corev1.PersistentVolume, error) {
+	return k.CoreV1().PersistentVolumes().Get(pvName, metav1.GetOptions{})
+}
+
+func (k *KubeClient) deletePV(pvName string) error {
+	return k.CoreV1().PersistentVolumes().Delete(pvName, &metav1.DeleteOptions{})
+}
+
 func (k *KubeClient) createDeployment(deployment *appsv1.Deployment) error {
 	_, err := k.AppsV1().Deployments(deployment.Namespace).Create(deployment)
 	if err != nil {
@@ -261,7 +269,7 @@ func (k *KubeClient) applyDeployment(deployment *appsv1.Deployment) error {
 		return err
 	}
 
-	// Patch the depployment
+	// Patch the deployment
 	_, err = k.AppsV1().
 		Deployments(deployment.Namespace).
 		Patch(deployment.Name,
@@ -293,7 +301,7 @@ func (k *KubeClient) listDeployments(namespace, labelSelector string) (*appsv1.D
 
 func dumpK8sObject(obj runtime.Object) {
 	if encoder == nil {
-		fmt.Printf("encoder not initilized\n")
+		fmt.Printf("encoder not initialized\n")
 		return
 	}
 
@@ -319,6 +327,10 @@ func (k *KubeClient) deleteStorageClass(scName string) error {
 // Add Kubernetes service related operations
 func (k *KubeClient) getService(namespace, name string) (*corev1.Service, error) {
 	return k.CoreV1().Services(namespace).Get(name, metav1.GetOptions{})
+}
+
+func (k *KubeClient) deleteService(namespace, name string) error {
+	return k.CoreV1().Services(namespace).Delete(name, &metav1.DeleteOptions{})
 }
 
 // Add Node related operations
@@ -372,7 +384,7 @@ func (k *KubeClient) waitForDeploymentRollout(ns, deployment string) error {
 				return false, nil
 			}
 			// if deploy.Status.Replicas > deploy.Status.UpdatedReplicas then some of the older replicas are in running state
-			// because newer replicas are not in running state. It waits for newer replica to come into reunning state then terminate.
+			// because newer replicas are not in running state. It waits for newer replica to come into running state then terminate.
 			// It marked IsRolledout as false and update message accordingly
 			if deploy.Status.Replicas > deploy.Status.UpdatedReplicas {
 				fmt.Printf("Waiting for deployment rollout to finish: %d old replicas are pending termination\n",

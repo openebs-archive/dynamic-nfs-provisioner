b4854ebb505e24ed29de230ce3ba1347fe5bf9b9
mayank <mayank.patel@mayadata.io>
2021-07-28 20:20:51 +0530

    updating go module files
    Signed-off-by: mayank <mayank.patel@mayadata.io>

--- buildscripts/check-diff.sh
+++ buildscripts/check-diff.sh
@@ -22,5 +22,5 @@ TEST_NAME=$1
 
 
 if [[ `git diff --shortstat | wc -l` != 0 ]]; then 
-  echo "Some files got changed after $1";printf "\n";git diff --stat;printf "\n"; exit 1; 
+  echo "Some files got changed after $1";printf "\n";git --no-pager diff;printf "\n"; exit 1;
 fi
